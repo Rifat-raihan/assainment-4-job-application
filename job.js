87b53dbbdca2b38d59bb5c6cccddd121f1af45b7
md.rifat hossain <mdrifat.tp88@gmail.com>
2026-02-24 20:20:58 +0600

    empty and count function

--- job.js
+++ job.js
@@ -1,3 +1,7 @@
+let rejectList = [];
+let interviewList = [];
+let currentStatus = 'all-btn';
+
 const allBtn = document.getElementById('all-btn');
 const interviewBtn = document.getElementById('interview-btn');
 const rejectedBtn = document.getElementById('rejected-btn');
@@ -7,3 +11,27 @@ const interviewCount = document.getElementById('interview');
 const cardSection = document.getElementById('all-card');
 const filterSection = document.getElementById('filterd-section');
 const mainContainer = document.querySelector('main');
+
+
+function getEmptyStateHTML() {
+    return `
+        <div class="flex flex-col items-center justify-center py-20 bg-white border border-gray-100 rounded-xl w-full">
+            <img src="jobs.png" alt="No jobs" class="w-24 mb-4">
+            <h2 class="text-2xl font-bold text-[#0f172a]">No jobs available</h2>
+            <p class="text-gray-500 mt-2">Check back soon for new job opportunities</p>
+        </div>
+    `;
+}
+
+function calculateCount() {
+ 
+    const totalCards = cardSection.querySelectorAll('.card').length;
+    
+    totalCount.innerText = totalCards;
+    interviewCount.innerText = interviewList.length;
+    rejectCount.innerText = rejectList.length;
+    
+    if (currentStatus === 'all-btn' && totalCards === 0) {
+        cardSection.innerHTML = getEmptyStateHTML();
+    }
+}
